e36727d1160340693e11429a31aac2538db6cbae
bittu9835 <bk2671111@gmail.com>
2023-11-27 14:35:38 +0530

    add

--- DB/DBconnection.ts
+++ DB/DBconnection.ts
@@ -1,13 +1,20 @@
 import mongoose from 'mongoose';
-import {ENV} from "../dotenv";
+import { ENV } from '../dotenv';
+
 const mongoURI = `${ENV.DB_HOST}/${ENV.DB_NAME}`;
-const connectionProperties:any = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
-mongoose.connect(mongoURI, connectionProperties).then((response:any)=>{
-    console.log('MongoDB Connection Succeeded.')
-}).catch((error:any)=>{
-    console.log('Error in DB connection: ' + error)
-});
+const connectionProperties: any = {
+//   useCreateIndex: true,s
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(mongoURI, connectionProperties)
+  .then((response: any) => {
+    console.log('MongoDB Connection Succeeded.');
+  })
+  .catch((error: any) => {
+    console.log('Error in DB connection: ' + error);
+  });
+
 export default mongoose;
